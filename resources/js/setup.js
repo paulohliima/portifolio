1fe907d103a8851ef3e2118eab82597c0daf12eb
Paulo Lima <paulo.lima-16@hotmail.com>
2021-08-24 13:03:34 -0300

    Portifólio com Elementos HTML,CSS,JAVA E JQUERY

---
+++ resources/js/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        },
+        1200:{
+            items:4
+        }
+    }
+})
